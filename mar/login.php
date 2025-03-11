df0932c36aa0e7fae895ae553d84dfcc5effe614
Camila <Camila.leite.oliveira@gmail.com>
2025-03-11 09:38:29 -0300

    Alterações junto a turma 2 AMS B

---
+++ mar/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Bluezão 2 AMS B</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+    <div class="w3-container">
+        <form action="index.php" method="post">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" required>
+            </p>
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha" required>
+            </p>
+            <button type="submit" value="Enviar">
+        </form>
+    </div>
+</body>
+</html>
